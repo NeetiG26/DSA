194b0876e86526b2d9ae37f81cb8e8a030cca24a
NeetiG26 <goenkaneeti@gmail.com>
2021-03-08 21:01:54 +0530

    Update merge_sorted_arrays_v2.py

--- Arrays/merge_sorted_arrays_v2.py
+++ Arrays/merge_sorted_arrays_v2.py
@@ -20,8 +20,29 @@ def brute_force(arr1, m, arr2, n):
 	arr1.sort()
 	return arr1
 
+def optimized_soln(arr1, m, arr2, n):
+	'''
+	Optimized Soltution O(m+n)
+	'''
+	nums1_c = arr1[:m]
+	p1 = 0
+	p2 = 0
+    
+	for i in range(n+m):
+		if (p1<m and nums1_c[p1] <= arr2[p2]) or p2>n:
+			arr1[i] = nums1_c[p1]
+			p1 = p1+1
+		else:
+			arr1[i] = arr2[p2]
+			p2 = p2 + 1
+	return arr1
+
+
 s1 = time()
 print('Brute Force: O((m+n)log(m+n)),  Space complexity O(1)')
 brute_force(nums1,m,nums2,n)
 print('Time Taken: ',(time()-s1))
-
+s2 = time()
+print('Optimized Soln: Time Complexity O(m+n),  Space complexity O(m)')
+optimized_soln(nums1,m,nums2,n)
+print('Time Taken: ',(time()-s2))
