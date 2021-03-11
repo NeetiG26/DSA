08e5070d248f27c8b6c732b45713a70441509e7c
NeetiG26 <goenkaneeti@gmail.com>
2021-03-11 14:13:54 +0530

    Updated the optimized soln

--- Arrays/double_exists.py
+++ Arrays/double_exists.py
@@ -15,7 +15,25 @@ def brute_force(arr):
 	return False
 
 
+def optimized_soln(arr):
+	'''
+	More optimized soltuion (O(n)), Space Complexity O(n)
+
+	'''
+	seen = set()
+	for i in arr:
+	# if 2 * i in seen or i % 2 == 0 and i // 2 in seen:
+		if 2 * i in seen or i / 2 in seen: 
+			return True
+		seen.add(i)
+	return False
+
+
 s1 = time()
 print('Brute Force: O(n^2) solution, Space O(1)')
 print(brute_force(nums))
 print('Time Taken: ',(time()-s1))
+s2 = time()
+print('Optimized Soln: O(n) solution, O(n) Space complexity')
+print(optimized_soln(nums))
+print('Time Taken: ',(time()-s2))
