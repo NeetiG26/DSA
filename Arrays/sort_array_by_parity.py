dd5b1ac8d9d4622487799497b6e037ca3471285a
NeetiG26 <goenkaneeti@gmail.com>
2021-03-17 18:48:07 +0530

    Update sort_array_by_parity.py

--- Arrays/sort_array_by_parity.py
+++ Arrays/sort_array_by_parity.py
@@ -2,6 +2,8 @@
 Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
 
 You may return any answer array that satisfies this condition.
+
+Any sequence is accepted between same parity numbers
 '''
 
 from time import time
