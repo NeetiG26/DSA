94fe530e726f86894567b4d0e334ed742b911b08
NeetiG26 <goenkaneeti@gmail.com>
2021-07-05 22:14:03 +0530

    Update isomorphic_strings.py

--- HashMaps/isomorphic_strings.py
+++ HashMaps/isomorphic_strings.py
@@ -17,3 +17,25 @@ def isIsomorphic(s: str, t: str) -> bool:
                 is_isomorphic = False
                 
         return is_isomorphic
+
+
+def better_soln(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    elif len(s)==len(t)==1:
+        return True
+    else:
+        map_s_t = {}
+        map_t_s = {}
+        is_isomorphic = True
+        for i in range(len(s)):
+            if (s[i] not in map_s_t.keys()) and (t[i] not in map_t_s.keys()):
+                map_s_t[s[i]] = t[i]
+                map_t_s[t[i]] = s[i]
+            elif (s[i] not in map_s_t.keys()) ^ (t[i] not in map_t_s.keys()):
+                is_isomorphic = False
+            elif (map_s_t[s[i]]!= t[i]) or (map_t_s[t[i]] !=s[i]):
+                
+                is_isomorphic = False
+                
+        return is_isomorphic
